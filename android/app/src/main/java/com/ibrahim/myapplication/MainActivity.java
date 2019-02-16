f2d6941f70b1f8929c277035e78bf820910edc8c
brofcb <33383212+brofcb@users.noreply.github.com>
2019-02-16 15:18:03 -0600

    Second Commit

--- android/app/src/main/java/com/ibrahim/myapplication/MainActivity.java
+++ android/app/src/main/java/com/ibrahim/myapplication/MainActivity.java
@@ -1,15 +1,55 @@
 package com.ibrahim.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    Button recordBtn, stopRecordBtn, playBtn, stopPlayBtn;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+
+    final int REQUEST_PREMISSION_CODE = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkPermissionFromDevice())
+        {
+
+        }
+        else
+        {
+            requestPermissions();
+        }
+
+    }
+
+    private void requestPermissions()
+    {
+        ActivityCompat.requestPermissions(this, new String[]
+        {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO
+
+        }, REQUEST_PREMISSION_CODE);
+    }
+
+    private boolean checkPermissionFromDevice()
+    {
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_storage_result== PackageManager.PERMISSION_GRANTED && record_audio_result==PackageManager.PERMISSION_GRANTED;
     }
 }
